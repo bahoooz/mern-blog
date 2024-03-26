03869ea68f4ae848d800d0d77de59146a646315c
Bahoz <julienbraultcherif@gmail.com>
2024-03-26 17:35:25 +0100

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ mongoose
   });
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
@@ -22,3 +22,9 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internal Server Error";
+  res.status(statusCode).json({ success: false , statusCode, message});
+});
