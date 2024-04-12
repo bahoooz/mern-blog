cbc7bb168ff14ca375e2a095ad97ca64e5d99817
Bahoz <julienbraultcherif@gmail.com>
2024-04-12 12:46:40 +0200

    complete signin page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -51,7 +51,7 @@ export default function SignUp() {
             Blog
           </Link>
           <p className="text-sm mt-5">
-            This is a demo project. You can sign yp with your email and password
+            This is a demo project. You can sign up with your email and password
             or with Google.
           </p>
         </div>
