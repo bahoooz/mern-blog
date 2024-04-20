790932f0863801aeb7ea916c7f3f6dc8025e9a5f
Bahoz <julienbraultcherif@gmail.com>
2024-04-20 02:27:46 +0200

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -17,7 +18,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
@@ -25,9 +26,10 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 app.use((error, req, res, next) => {
   const statusCode = error.statusCode || 500;
   const message = error.message || "Internal Server Error";
-  res.status(statusCode).json({ success: false , statusCode, message});
+  res.status(statusCode).json({ success: false, statusCode, message });
 });
