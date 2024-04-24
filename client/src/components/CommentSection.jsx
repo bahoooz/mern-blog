2e121b6d41f00e440c93789c501e94fd5432b213
Bahoz <julienbraultcherif@gmail.com>
2024-04-25 00:23:12 +0200

    add edit functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -11,7 +11,7 @@ export default function CommentSection({ postId }) {
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   console.log(comments);
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -59,21 +59,37 @@ export default function CommentSection({ postId }) {
   const handleLike = async (commentId) => {
     try {
       if (!currentUser) {
-        navigate("/sign-in")
+        navigate("/sign-in");
         return;
       }
       const res = await fetch(`/api/comment/likeComment/${commentId}`, {
-        method: "PUT"
-      })
+        method: "PUT",
+      });
       if (res.ok) {
-        const data = await res.json()
-        setComments(comments.map((comment) => (
-          comment._id === commentId ? {...comment, likes: data.likes, numberOfLikes: data.likes.length} : comment
-        )))
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
       }
     } catch (error) {
       console.log(error.message);
     }
+  };
+
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
   };
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
@@ -138,7 +154,7 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit} />
           ))}
         </>
       )}
