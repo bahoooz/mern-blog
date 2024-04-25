6451aa73343b7d71a54b9b3eb3ead4da80d13936
Bahoz <julienbraultcherif@gmail.com>
2024-04-25 23:01:32 +0200

    test dist 2

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.use((error, req, res, next) => {
   const statusCode = error.statusCode || 500;
